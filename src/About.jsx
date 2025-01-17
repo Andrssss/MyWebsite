3492c229eebac55c43d79bc6805bde3883879d60
Andris <bak.andrs@gmail.cpom>
2025-01-17 22:43:37 +0100

    eeeee

--- src/About.jsx
+++ src/About.jsx
@@ -3,19 +3,11 @@ import './About.css';
 
 const About = () => {
   return (
-    
   <div className="about-main">
-
-
-
-
       <div className="about-container">
         <h2>Kapcsolat</h2>
-        <p>Ha szeretnél elérni, vagy felkerülni a listára vagy anygot megosztani vagy esetleg frisebb véleményt megosztani, akkor írj emailt: <a href="mailto:bak.andrs@gmail.com">bak.andrs@gmail.com</a></p>
-        {status && <p className="status-message">{status}</p>}
-
-
-        
+        <p>Ha szeretnél elérni, vagy felkerülni a listára vagy anygot megosztani. Esetleg frissebb véleményt megosztani, akkor írj emailt: <a href="mailto:bak.andrs@gmail.com">bak.andrs@gmail.com </a></p>
+        <p>Az oldal fejlesztés alatt van, még itt-ott változni fog. </p>
       </div>
     </div>
   );
