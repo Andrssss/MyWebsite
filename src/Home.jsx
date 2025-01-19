0c75ed8d5c0d73d8e3d3ff3a54da8a5b48a2c5b7
Andris <bak.andrs@gmail.cpom>
2025-01-19 13:48:24 +0100

    important

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Home = ({ folders, currentPath, handleMenuClick, setCurrentPath }) => {
   const folderId = '1V4ryxSmLNoQZ14UQry1DbfjUlVWN6DTx'; // Cseréld ki a Google Drive mappád azonosítójára
   const embedLink = `https://drive.google.com/embeddedfolderview?id=${folderId}#grid`;
+
+  // Hook a képernyő szélességének figyelésére
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768); // Beállítja, ha mobil képernyő
+    };
+
+    // Inicializálás
+    handleResize();
+
+    // Eseményfigyelő hozzáadása
+    window.addEventListener('resize', handleResize);
+
+    // Takarítás
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
   return (
     <div
       style={{
         width: '110%',
         height: '90vh', // Ensures full viewport height for iframe
-        marginLeft: '-10%', // Balra tolás, pl. -10% -kal
-
+        marginLeft: isMobile ? '-10%' : '0', // Ha mobil, akkor -10% margó, különben nincs margó
       }}
     >
       <iframe
