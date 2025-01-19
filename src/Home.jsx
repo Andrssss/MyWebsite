aa04c3536897055016db8cf5eea20b35fecc942f
Andris <bak.andrs@gmail.cpom>
2025-01-19 13:36:46 +0100

    iframe igazitás

--- src/Home.jsx
+++ src/Home.jsx
@@ -7,15 +7,20 @@ const Home = ({ folders, currentPath, handleMenuClick, setCurrentPath }) => {
   return (
     <div
       style={{
-        width: '110%',
-        height: '90vh', // Ensures full viewport height for iframe
+        width: '90%', // Csökkentett szélesség
+        height: '80vh', // Csökkentett magasság
+        margin: '0 auto', // Központosítás
+        padding: '70px', // Csökkentett térköz a tartalom körül
+        paddingTop: '0px'
+
+        // Ha még közelebb szeretnéd a fejléchez, csökkentheted a padding-top-ot is
       }}
     >
       <iframe
         src={embedLink}
         style={{
-          width: '110%', // Teljes szélesség
-          height: '100%', // Teljes magasság
+          width: '120%', // Teljes szélesség a div-en belül
+          height: '120%', // Teljes magasság a div-en belül
           border: 'none',
           overflow: 'hidden',
         }}
