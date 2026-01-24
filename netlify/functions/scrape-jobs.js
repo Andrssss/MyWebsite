5c004b8b4d5703d73947482aa502ce5b4246094a
Andris Bakó <andris@example.com>
2026-01-24 19:09:43 +0100

    sd

--- netlify/functions/scrape-jobs.js
+++ netlify/functions/scrape-jobs.js
@@ -4,6 +4,7 @@ exports.handler = async () => {
     headers: { "Content-Type": "application/json; charset=utf-8" },
     body: JSON.stringify({
       ok: true,
+      marker: "SCRAPE_JOBS_MINIMAL_2026-01-24_XX1",  // <- ezt látni kell
       node: process.version,
     }),
   };
