b50bc74611af59c305b961e5691a1684d4b77c4c
Andris Bakó <andris@example.com>
2026-01-24 19:14:35 +0100

    scrape

--- netlify/functions/ping.js
+++ netlify/functions/ping.js
@@ -2,6 +2,10 @@ exports.handler = async () => {
   return {
     statusCode: 200,
     headers: { "Content-Type": "application/json; charset=utf-8" },
-    body: JSON.stringify({ ok: true, ping: true, node: process.version }),
+    body: JSON.stringify({
+      ok: true,
+      marker: "SCRAPE_JOBS_MINIMAL_2026-01-24_XX1",  // <- ezt látni kell
+      node: process.version,
+    }),
   };
 };
--- netlify/functions/scrape_jobs.js
+++ netlify/functions/scrape_jobs.js
@@ -4,7 +4,7 @@ exports.handler = async () => {
     headers: { "Content-Type": "application/json; charset=utf-8" },
     body: JSON.stringify({
       ok: true,
-      marker: "SCRAPE_JOBS_MINIMAL_2026-01-24_XX1",  // <- ezt látni kell
+      marker: "SCRAPE_JOBS_NEW_NAME",
       node: process.version,
     }),
   };
