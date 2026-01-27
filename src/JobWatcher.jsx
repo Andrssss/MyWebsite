a27538e8888f8a0a7b99de13dac259edd5c4844a
Andrssss <132691684+Andrssss@users.noreply.github.com>
2026-01-27 22:35:51 +0100

    Update JobWatcher.jsx

--- src/JobWatcher.jsx
+++ src/JobWatcher.jsx
@@ -127,10 +127,10 @@ const JobWatcher = () => {
         <div>
           <h3>
             <p>
-                Automata scraper. Minden nap UTC szerint 4 illetve 14-re fut le. Ekkor frissül.
+                Automata scraper. Minden nap UTC szerint 4 illetve 14-kor fut. Ekkor frissül.
             </p>
             <p>
-              Elég kakán néz ki maszív fejlesztés alatt van. Ötleteket meghallgatok, de sajnos egy nehéz és lassú folyamat, ezért kérlek donétolj 500ft -t. xd  
+              Elég kakán néz ki, maszív fejlesztés alatt van. Ötleteket meghallgatok, de sajnos egy nehéz és lassú folyamat, ezért kérlek donétolj 500ft -t. xd  
             </p>
             <h4>
               <p>
