990d1ce3f34a5100fec7f7e01730b8b112414c1b
Andris <Andris@DESKTOP-VO2F358>
2026-05-01 13:13:37 +0200

    [jobs] Profession biiig fix, like hella big

--- netlify/functions/cron_jobs_P.mjs
+++ netlify/functions/cron_jobs_P.mjs
@@ -18,6 +18,7 @@ const TASKS = [
   { jobName: "P_4", url: "https://www.profession.hu/allasok/programozo-fejleszto/budapest/1,10,23,0,75" },
   { jobName: "P_5", url: "https://www.profession.hu/allasok/tesztelo-tesztmernok/budapest/1,10,23,0,80" },
   { jobName: "P_6", url: "https://www.profession.hu/allasok/it-programozas-fejlesztes/budapest/1,10,23,intern" },
+  { jobName: "P_7", url: "https://www.profession.hu/allasok/budapest/1,1_2_10_18_25_28,23,0,1_2_6_7_9_11_16_61_69_70_72_73_75_76_77_78_79_80_85_100_144_146_148_197_200_201_202_239_282_283_338_353_361_363_365_375_393_426_427_432_433_448_449_450_453,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1" }
 ];
 
 export default withTimeout("cron_jobs_P", async () => {
